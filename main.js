4e04dcbb3bfda15239669c0be95e9e941d794d91
Nacho <nachomigoni@gmail.com>
2022-11-07 21:05:06 -0300

    Casi terminado

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 class Reclamo{
     constructor(id,nombre,email,telefono,sector,descripcion,categoria){
         this.nombre = nombre
@@ -20,17 +21,16 @@ class Reclamo{
     }
 }
 
+var confirmacion;
+
 let reclamos = []
 function principal(){
     let nombre = document.getElementById("nombre").value
     let email = document.getElementById("email").value
     let telefono = document.getElementById("telefono").value
-    do{
-        let sector = document.getElementById("sector").value
-        let descripcion = document.getElementById("descripcion").value
-        carga_reclamo()
-        repeticion()
-    }while(confirmacion)
+    let sector = document.getElementById("sector").value
+    let descripcion = document.getElementById("descripcion").value
+    carga_reclamo()
 }
 
 
@@ -38,14 +38,23 @@ function carga_reclamo(){
     let reclamo = new Reclamo(nombre,email,telefono,sector,categoria,descripcion);
     reclamos.push(reclamo)
     alert("Reclamo enviado exitosamente!")
-    let confirmacion = confirm("Realizar otro reclamo?")
+    confirmacion = confirm("Realizar otro reclamo?")
+    if(confirmacion){
+        repeticion()
+    }else{
+        location.assign('muestra_reclamos.html')
+        vista()
+    }
 }
 
-function repetecion(){
+function repeticion(){
     if (confirmacion){
-        document.getElementById("nombre").innerHTML = `<input type="text" class="form-control" value=${nombre} disabled>`
-        document.getElementById("email").innerHTML = `<input type="text" class="form-control" value=${email} disabled>`
-        document.getElementById("telefono").innerHTML = `<input type="text" class="form-control" value=${telefono} disabled>`
+        let nombre = document.getElementById("nombre")
+        nombre.setAttribute("disabled","true")
+        document.getElementById("email").setAttribute("disabled","true")
+        document.getElementById("telefono").setAttribute("disabled","true")
+        document.getElementById("sector").value = ""
+        document.getElementById("descripcion").value = ""
         document.getElementById("tipo").innerHTML = `
         <select class="form-select" id="tipo">
             <option value="alumbrado">Alumbrado</option>
@@ -55,13 +64,15 @@ function repetecion(){
             <option value="mantenimiento">Mantenimiento de calles</option>
             <option value="otros">Otros</option>
         </select>`
+        document.getElementById("tipo").removeAttribute("disabled")
     }else{
-        location.href('muestra_reclamos.html')
+        location.assign('muestra_reclamos.html')
+        vista()
     }
 }
 
 function vista(){
-    document.getElementById(cuerpo).innerHTML += reclamo.datos_usuario();
+    document.getElementById("cuerpo").innerHTML += reclamo.datos_usuario();
     for (let i = 0; i < reclamos.length; i++) {
         const element = reclamos[i];
         document.getElementById(cuerpo).innerHTML += `
@@ -73,4 +84,26 @@ function vista(){
             </div>
         </div>`
     }
+}
+
+var categoria = null
+
+function def_alumbrado(){
+categoria = "alumbrado"
+console.log(categoria)
+}
+function def_bacheo(){
+categoria = "bacheo"
+}
+function def_r_ramas(){
+categoria = "r_ramas"
+}
+function def_r_residuos(){
+categoria = "r_residuos"
+}
+function def_mantenimiento(){
+categoria = "mantenimiento"
+}
+function def_otro(){
+categoria = "otro"
 }
